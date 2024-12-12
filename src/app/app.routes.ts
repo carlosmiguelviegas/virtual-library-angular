9a27736762953e1a9478d7aaaab3dcae8adc0c34
unknown <carlosmviegas86@gmail.com>
2024-12-12 00:05:09 +0000

    added routing and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
+  { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule) },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'books',
+  standalone: true,
+  imports: [],
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent implements OnInit {
+
+  ngOnInit(): void {
+    // it was intentional
+  }
+
+}
