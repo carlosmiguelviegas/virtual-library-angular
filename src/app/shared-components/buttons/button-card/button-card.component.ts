655ae7a89d1f5dcac3202892d7a0a9081b8c1730
unknown <carlosmviegas86@gmail.com>
2024-12-17 13:57:23 +0000

    added a customable tooltip to the button card component

--- src/app/shared-components/buttons/button-card/button-card.component.ts
+++ src/app/shared-components/buttons/button-card/button-card.component.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+
 import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'button-card',
   standalone: true,
-  imports: [ CommonModule, MatIconModule, MatIcon ],
+  imports: [ CommonModule, MatIconModule, MatIcon, MatTooltipModule ],
   templateUrl: './button-card.component.html',
   styleUrl: './button-card.component.css'
 })
 export class ButtonCardComponent {
 
   @Input() icon!: string;
+  @Input() tooltip!: string;
   @Output() onClick: EventEmitter<any> = new EventEmitter();
 
   onButtonClick = (_event: any) => this.onClick.emit();
