6e01a507490e21d674b5154ffdc8a03b1564fe47
unknown <carlosmviegas86@gmail.com>
2024-12-21 20:09:25 +0000

    added a reusable component to display and hide the password

---
+++ src/app/shared-components/inputs/display-and-hide-password/display-and-hide-password.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'display-and-hide-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-and-hide-password.component.html',
+  styleUrl: './display-and-hide-password.component.css'
+})
+export class DisplayAndHidePasswordComponent implements OnInit {
+
+  ngOnInit(): void {
+    // it was intentional
+  }
+
+}
