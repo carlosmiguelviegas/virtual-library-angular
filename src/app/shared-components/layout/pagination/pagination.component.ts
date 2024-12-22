c524fc852b8f55da7eb06ce60108f74ad4f8507a
unknown <carlosmviegas86@gmail.com>
2024-12-22 13:19:21 +0000

    added a reusable pagination component

---
+++ src/app/shared-components/layout/pagination/pagination.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navigation-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation-bar.component.html',
+  styleUrl: './navigation-bar.component.css'
+})
+export class NavigationBarComponent implements OnInit {
+
+  ngOnInit(): void {
+    // it was intentional
+  }
+
+}
